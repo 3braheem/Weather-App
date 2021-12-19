0b97e1f17715fe8e50e9ad0e3abbeff4462cbe93
Ibrahim <94210025+hunafa@users.noreply.github.com>
2021-12-19 12:35:34 -0800

    Initial version

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'tachyons';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* A weather app written in React/Typescript */}
+      <h1 className='tc f1'>TempCheck</h1>
+    </div>
+  );
+}
+
+export default App;
