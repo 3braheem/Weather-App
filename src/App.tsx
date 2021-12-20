99073fb58ea6e150b8f828c6aaa1bb544f4d3854
Ibrahim <94210025+hunafa@users.noreply.github.com>
2021-12-20 10:28:35 -0800

    Skeleton for homepage UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* A weather app written in React/Typescript */}
-      <h1 className='tc f1'>TempCheck</h1>
+    <div className='container'>
+      <div className="App">
+        {/* A weather app written in React/Typescript */}
+        <h1 className='tl f1 ma5'>Find out how the weather is looking.</h1>
+        <div className='box'>
+            <h2 className='tc f3 ma2 pa3'>Search for a city or town.</h2>
+            <div className='searchfield'>
+              <input type="text" className='search' />
+              <button>search</button>
+            </div>
+            <div className='forecastOption'>
+              <button>Hourly</button>
+              <button>7 day</button>
+            </div>  
+        </div>
+      </div>
     </div>
   );
 }
